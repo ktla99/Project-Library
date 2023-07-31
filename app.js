45e7d1ec4f165d405dfaa891d1f7de0ec1ff002d
Kim <ktla3223@gmail.com>
2023-07-31 14:48:00 -0700

    Added to DOMContentLoaded for button for titleOfBook, numberOfPages, readQuestion, and notReadQuestion

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ window.onload = function() {
     document.getElementById('checkboxTwo').style.display = 'none';
     document.getElementById('titleOfBook').style.display = 'none';
     document.getElementById('numberOfPages').style.display = 'none';
-    document.getElementsByClassName('numberOfPages')[0].style.display = 'none';
+    document.getElementById('readQuestion').style.display = 'none';
+    document.getElementById('notReadQuestion').style.display = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -35,6 +36,10 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('pages').style.display = '';
         document.getElementById('checkboxOne').style.display = '';
         document.getElementById('checkboxTwo').style.display = '';
+        document.getElementById('titleOfBook').style.display = '';
+        document.getElementById('numberOfPages').style.display = '';
+        document.getElementById('readQuestion').style.display = '';
+        document.getElementById('notReadQuestion').style.display = '';
     });
 });
 
