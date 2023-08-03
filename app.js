92214ff8b9b3968d18f5e32686dba1cc5d3d3988
Kim <ktla3223@gmail.com>
2023-08-03 10:41:46 -0700

    Removed excess prior information

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 let myLibrary = [];
 
-    function Book(name, pages, read) {
-        let book = Object.create(Book.prototype)
-        this.name = name
-        this.pages = pages
-        this.read = read
-
-        return book
+    function Book() {
+        constructor() {}
     }
 
     function addBookToLibrary() {
@@ -45,46 +40,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-    /*
-    const title = "Warriors"
-    const author = "Erin Hunter"
-    const pages = "288";
-    const read = "have read"
 
-    const personOne = {
-        title: "The Call of Cthulu and Other Weird Stories",
-        author: "H.P. Lovecraft",
-        pages: "420",
-        read: "have not read"
-    }
-
-    const personTwo = {
-        title: "Warriors",
-        author: "Erin Hunter",
-        pages: "288",
-        read: "have read"
-    }
-
-    function printPerson(person) {
-        console.log(person.title)
-    }
-
-    printPerson(personTwo);
-    
-
-    function Person(title, author, pages, read) {
-        this.title = title
-        this.author = author
-        this.pages = pages 
-        this.read = read 
-        this.info = function() {
-            console.log(title, author, pages, read)
-        }
-    }
-
-    const warriors = new Person('"Warriors, by', ' Erin Hunter,', ' 288 pages,', ' have read already"')
-
-    warriors.info()
-
-    console.log(warriors.info());
-    */
