91e0b618d37a724525a47b233f53e71125e9c409
Kim <ktla3223@gmail.com>
2023-08-03 12:50:06 -0700

    Added function Error and libraryButton

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 let myLibrary = [];
 
     function Book() {
-        constructor() {}
+        constructor()
     }
 
     function addBookToLibrary() {
+        const libraryButton = document.getElementById("bookSubmit");
+        libraryButton.addEventListener('click', ()=> {
+            const newLibraryBook = document.body.appendChild(document.createElement("p"));
+            document.body.appendChild(p);
+        })
+    };
 
-        for (let key in Book) {
-            console.log()
-        }
-    }
+function Error() {
+    
+}
+
+
 
 
 window.onload = function() {
@@ -41,3 +48,47 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
+    /*
+    const title = "Warriors"
+    const author = "Erin Hunter"
+    const pages = "288";
+    const read = "have read"
+
+    const personOne = {
+        title: "The Call of Cthulu and Other Weird Stories",
+        author: "H.P. Lovecraft",
+        pages: "420",
+        read: "have not read"
+    }
+
+    const personTwo = {
+        title: "Warriors",
+        author: "Erin Hunter",
+        pages: "288",
+        read: "have read"
+    }
+
+    function printPerson(person) {
+        console.log(person.title)
+    }
+
+    printPerson(personTwo);
+    
+
+    function Person(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages 
+        this.read = read 
+        this.info = function() {
+            console.log(title, author, pages, read)
+        }
+    }
+
+    const warriors = new Person('"Warriors, by', ' Erin Hunter,', ' 288 pages,', ' have read already"')
+
+    warriors.info()
+
+    console.log(warriors.info());
+    */
